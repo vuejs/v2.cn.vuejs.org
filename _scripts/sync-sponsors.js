0f922c4fc67116b3a0f8320f56a8d65866f87824
Evan You <yyx990803@gmail.com>
2022-07-26 12:09:51 +0800

    fix special sponsor dynamic switch logic

--- _scripts/sync-sponsors.js
+++ _scripts/sync-sponsors.js
@@ -8,6 +8,17 @@ const configPath = path.resolve(__dirname, '../themes/vue/_config.yml')
 
 ;(async () => {
   const { data } = await axios(`https://sponsors.vuejs.org/data.json`)
+
+  const cnSponsors = data.platinum_china
+
+  // DCloud special switch logic
+  const s = cnSponsors.find(s => s.name === 'HBuilder')
+  if (s) {
+    s.ad_img = 'dcloud1.png'
+    s.alt_img = 'dcloud2.png'
+    s.height = 70
+  }
+
   const yml = yaml.dump(data)
   const config = fs.readFileSync(configPath, 'utf-8')
   const updated = config.replace(
