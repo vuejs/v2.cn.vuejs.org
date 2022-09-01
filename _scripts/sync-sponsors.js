a1645184abbdd06401856f68359de41236a4d6db
Evan You <yyx990803@gmail.com>
2022-09-01 09:47:14 +0800

    update sposnors

--- _scripts/sync-sponsors.js
+++ _scripts/sync-sponsors.js
@@ -9,17 +9,6 @@ const configPath = path.resolve(__dirname, '../themes/vue/_config.yml')
 ;(async () => {
   const { data } = await axios(`https://sponsors.vuejs.org/data.json`)
 
-  const cnSponsors = data.platinum_china
-
-  // DCloud special switch logic
-  const s = cnSponsors.find((s) => s.name === 'HBuilder')
-  if (s) {
-    s.ad_img = 'dcloud1.png'
-    s.alt_img = 'dcloud2.png'
-    s.height = 70
-    s.url = `https://www.dcloud.io/?hmsr=vuejsorg&hmpl=&hmcu=&hmkw=&hmci=`
-  }
-
   const yml = yaml.dump(data)
   const config = fs.readFileSync(configPath, 'utf-8')
   const updated = config.replace(
